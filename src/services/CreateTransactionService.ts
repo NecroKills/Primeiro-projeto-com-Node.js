10e47189760f473350b3182d51a458f5f377d91f
Maycon <maycondasilvamoreira@hotmail.com>
2020-05-03 10:52:36 -0300

    subindo o desafio, foi feito o post da transaction, o getAll das transactions e o balance das transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    // Compara se o type possui qualuer um dos valores do array,
+    // Se ele incluir retorna true, se não retorna false.
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Transaction type is invalid.');
+    }
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw Error('The outcome transaction exceeded the total amount in cash.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
